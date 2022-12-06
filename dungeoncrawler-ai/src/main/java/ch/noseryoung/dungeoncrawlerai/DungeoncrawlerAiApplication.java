ed04857a97aefd075951741bf0ede0c06dc22d3a
MikkaK <mikka.kummer@noseryoung.ch>
2022-12-06 18:44:05 +0100

    added project files

---
+++ dungeoncrawler-ai/src/main/java/ch/noseryoung/dungeoncrawlerai/DungeoncrawlerAiApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.dungeoncrawlerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeoncrawlerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DungeoncrawlerAiApplication.class, args);
+    }
+
+}
---
+++ dungeoncrawler-ai/src/test/java/ch/noseryoung/dungeoncrawlerai/DungeoncrawlerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.dungeoncrawlerai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeoncrawlerAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
