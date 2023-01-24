3aee9ee5c45aeadca780274cfbcd2b03ab407250
Cedzol <cedric.zollinger@symbionet.io>
2023-01-24 11:46:19 +0100

    workign test

--- src/test/java/ch/noseryoung/dungeoncrawlerai/DungeoncrawlerAiApplicationTests.java
+++ src/test/java/ch/noseryoung/dungeoncrawlerai/DungeoncrawlerAiApplicationTests.java
@@ -1,13 +1,48 @@
 package ch.noseryoung.dungeoncrawlerai;
 
+import ch.noseryoung.dungeoncrawlerai.domain.balance.BalanceEntity;
+import ch.noseryoung.dungeoncrawlerai.domain.balance.GenerationLogic;
+import ch.noseryoung.dungeoncrawlerai.domain.enemy.EnemyEntityService;
+import ch.noseryoung.dungeoncrawlerai.domain.enemy.Entity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DungeoncrawlerAiApplicationTests {
 
+    @Autowired
+    EnemyEntityService entityService;
+
+    private BalanceEntity enemyDead = new BalanceEntity(
+            42,
+            70,
+            0,
+            "RoomFloppy",
+            "Floppy Diskette",
+            0,
+            177,
+            false,
+            2,
+            47,
+            0,
+            1,
+            1,
+            false,
+            0,
+            false,
+            0
+    );
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGetEntity() {
+        GenerationLogic logic = new GenerationLogic(entityService);
+
+        logic.calculateEnemyDeath(enemyDead);
     }
 
 }
